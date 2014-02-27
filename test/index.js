e8ed0e554c7be0e684d7929ecbe37fcbc7fb81bc
Asa Ayers <Asa@AsaAyers.com>
2014-02-26 21:33:35 -0500

    fix/add tests

--- test/index.js
+++ test/index.js
@@ -302,6 +302,7 @@ test('\nincrement versions test', function(t) {
     ['1.2.3tag', 'major', '2.0.0', true],
     ['1.2.3-tag', 'major', '2.0.0'],
     ['1.2.3', 'fake', null],
+    ['1.2.0-0', 'patch', '1.2.0'],
     ['fake', 'major', null],
     ['1.2.3-4', 'major', '2.0.0'],
     ['1.2.3-4', 'minor', '1.3.0'],
