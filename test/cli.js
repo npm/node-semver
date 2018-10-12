cb71dbbdd7d5418f0cbe6bef193d1e1fcc8f6dab
isaacs <i@izs.me>
2018-10-11 17:29:05 -0700

    refactor cli test to work without execSync

--- test/cli.js
+++ test/cli.js
@@ -1,33 +1,35 @@
-var tap = require('tap');
-var test = tap.test;
+'use strict'
+var t = require('tap');
 var execSync = require('child_process').execSync;
 
-// TODO: @bcoe debug our approach to spawning CLI on Windows.
-if (process.platform === 'win32') return
+const spawn = require('child_process').spawn
+const bin = require.resolve('../bin/semver')
+const run = args => new Promise((res, rej) => {
+  const c = spawn(process.execPath, [bin].concat(args))
+  c.on('error', rej)
+  const out = []
+  const err = []
+  c.stdout.setEncoding('utf-8')
+  c.stdout.on('data', chunk => out.push(chunk))
+  c.stderr.setEncoding('utf-8')
+  c.stderr.on('data', chunk => err.push(chunk))
+  c.on('close', (code, signal) => {
+    res({
+      out: out.join(''),
+      err: err.join(''),
+      code: code,
+      signal: signal
+    })
+  })
+})
 
-test('\nCLI (bin/semver) tests', function(t) {
-  // [args, expected stdout, expected stderr, expected error code]
+const runTest = (t, args, expect) =>
+  run(args).then(actual => t.match(actual, expect))
+
+t.test('inc tests', t => {
   [
-    ["-i major 1.0.0", "2.0.0"],
-    ["-i major 1.0.0 1.0.1", null, "--inc can only be used on a single version with no range", 1]
-  ].forEach(function (input) {
-
-    var expectedStdout = input[1];
-
-    try {
-      stdout = execSync('"./bin/semver" ' + input[0]);
-      if (expectedStdout) {
-        t.same(stdout.toString().trim(), expectedStdout);
-      }
-    } catch (err) {
-      var expectedStderr = input[2];
-      var expectedStatusCode = input[3];
-      if (expectedStatusCode) {
-        t.same(err.stderr.toString().trim(), expectedStderr);
-        t.same(err.status, expectedStatusCode);
-      }
-    }
-
-  });
-  t.done();
+    [['-i', 'major', '1.0.0'], { out: '2.0.0', code: 0, signal: null }],
+    [['-i', 'major', '1.0.0', '1.0.1'], { out: '', err: '--inc can only be used on a single version with no range', code: 1 }]
+  ].forEach(c => t.resolveMatch(run(c[0]), c[1]))
+  t.end()
 });
