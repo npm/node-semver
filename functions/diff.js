891b7dab49968768d88142c39778fa2427198ea0
Tom Jenkinson <tom@tjenkinson.me>
2023-04-11 14:45:15 -0700

    fix: incorrect diff when going from 0.0.0 prerelease to 0.0.0

--- functions/diff.js
+++ functions/diff.js
@@ -7,36 +7,45 @@ const diff = (version1, version2) => {
   const v2 = parse(version2)
   if (eq(v1, v2)) {
     return null
-  } else {
-    const highVersion = gt(v1, v2) ? v1 : v2
-    const hasPre = highVersion.prerelease.length
-    const prefix = hasPre ? 'pre' : ''
-    const defaultResult = hasPre ? 'prerelease' : ''
-
-    if (v1.major !== v2.major) {
-      return prefix + 'major'
-    }
-
-    if (v1.minor !== v2.minor) {
-      return prefix + 'minor'
-    }
-
-    if (v1.patch !== v2.patch) {
-      return prefix + 'patch'
-    }
-
-    if (!v1.prerelease.length || !v2.prerelease.length) {
-      if (v1.patch) {
-        return 'patch'
-      }
-      if (v1.minor) {
-        return 'minor'
-      }
-      if (v1.major) {
-        return 'major'
-      }
-    }
-    return defaultResult // may be undefined
   }
+
+  const v1Higher = gt(v1, v2)
+  const highVersion = v1Higher ? v1 : v2
+  const lowVersion = v1Higher ? v2 : v1
+  const highHasPre = !!highVersion.prerelease.length
+  const lowHasPre = !!lowVersion.prerelease.length
+  const prefix = highHasPre ? 'pre' : ''
+
+  if (v1.major !== v2.major) {
+    return prefix + 'major'
+  }
+
+  if (v1.minor !== v2.minor) {
+    return prefix + 'minor'
+  }
+
+  if (v1.patch !== v2.patch) {
+    return prefix + 'patch'
+  }
+
+  if (lowHasPre && !highHasPre) {
+    if (lowVersion.patch) {
+      // anything higher than a patch bump would result in the wrong version
+      return 'patch'
+    }
+
+    if (lowVersion.minor) {
+      // anything higher than a minor bump would result in the wrong version
+      return 'minor'
+    }
+
+    // bumping major/minor/patch all have same result
+    return 'major'
+  }
+
+  // at this point it must be that both are prereleases and
+  // prelease parts are different because we know the 2 versions
+  // are not equal
+  return 'prerelease'
 }
 module.exports = diff
