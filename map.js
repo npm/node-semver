0214eb6859494c3a41df7dcb5ad9a868faaffeeb
SpriteyOnTheDraconic <91148727+SpriteyOnTheDraconic@users.noreply.github.com>
2023-04-13 17:25:53 +0300

    Update map.js
    I replaced the arrow function with a named function for clarity and readability.
    I added a conditional statement to check if the testFile argument starts with the "test/" prefix before removing it. This avoids removing part of the file path if it doesn't contain the "test/" prefix.
    I defined the prefix variable to store the "test/" prefix for readability and maintainability.
    I moved the logic for removing the "test/" prefix to a separate function to make it reusable and testable.
    I exported the removeTestPrefix function at the end of the file to make it accessible to other modules.

--- map.js
+++ map.js
@@ -1,1 +1,8 @@
-module.exports = testFile => testFile.replace(/test\//, '')
+// Define a function that removes the "test/" prefix from a file path
+function removeTestPrefix(testFile) {
+const prefix = 'test/';
+return testFile.startsWith(prefix) ? testFile.substring(prefix.length) : testFile;
+}
+
+// Export the removeTestPrefix function
+module.exports = removeTestPrefix;
