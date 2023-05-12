b453496951918a489d659e9e7ec6fe8d3f64032a
Tom Jenkinson <tom@tjenkinson.me>
2023-05-12 16:23:58 +0100

    target specific dirs

--- .eslintrc.local.js
+++ .eslintrc.local.js
@@ -1,21 +1,17 @@
 'use strict'
 
 module.exports = {
-  rules: {
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: false,
-      },
-    ],
-    'import/no-nodejs-modules': ['error'],
-  },
   overrides: [
     {
-      files: ['**/test/**', '.eslintrc.js', '.eslintrc.local.js'],
+      files: ['bin/**', 'classes/**', 'functions/**', 'internal/**', 'ranges/**'],
       rules: {
-        'import/no-extraneous-dependencies': 0,
-        'import/no-nodejs-modules': 0,
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: false,
+          },
+        ],
+        'import/no-nodejs-modules': ['error'],
       },
     },
   ],
