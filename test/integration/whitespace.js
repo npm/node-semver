b209a9aa01a5e101851bdfcd78901cb245c9a7f1
Luke Karrys <luke@lukekarrys.com>
2023-06-22 11:39:02 -0700

    fix: set max lengths in regex for numeric and build identifiers

--- test/integration/whitespace.js
+++ test/integration/whitespace.js
@@ -7,33 +7,43 @@ const minVersion = require('../../ranges/min-version')
 const minSatisfying = require('../../ranges/min-satisfying')
 const maxSatisfying = require('../../ranges/max-satisfying')
 
-const s = (n = 500000) => ' '.repeat(n)
+const wsMedium = ' '.repeat(125)
+const wsLarge = ' '.repeat(500000)
+const zeroLarge = '0'.repeat(500000)
 
-test('regex dos via range whitespace', (t) => {
-  // a range with this much whitespace would take a few minutes to process if
+test('range with whitespace', (t) => {
+  // a range with these extra characters would take a few minutes to process if
   // any redos susceptible regexes were used. there is a global tap timeout per
   // file set in the package.json that will error if this test takes too long.
-  const r = `1.2.3 ${s()} <1.3.0`
-
+  const r = `1.2.3 ${wsLarge} <1.3.0`
   t.equal(new Range(r).range, '1.2.3 <1.3.0')
   t.equal(validRange(r), '1.2.3 <1.3.0')
   t.equal(minVersion(r).version, '1.2.3')
   t.equal(minSatisfying(['1.2.3'], r), '1.2.3')
   t.equal(maxSatisfying(['1.2.3'], r), '1.2.3')
+  t.end()
+})
 
+test('range with 0', (t) => {
+  const r = `1.2.3 ${zeroLarge} <1.3.0`
+  t.throws(() => new Range(r).range)
+  t.equal(validRange(r), null)
+  t.throws(() => minVersion(r).version)
+  t.equal(minSatisfying(['1.2.3']), null)
+  t.equal(maxSatisfying(['1.2.3']), null)
   t.end()
 })
 
 test('semver version', (t) => {
-  const v = `${s(125)}1.2.3${s(125)}`
-  const tooLong = `${s()}1.2.3${s()}`
+  const v = `${wsMedium}1.2.3${wsMedium}`
+  const tooLong = `${wsLarge}1.2.3${wsLarge}`
   t.equal(new SemVer(v).version, '1.2.3')
   t.throws(() => new SemVer(tooLong))
   t.end()
 })
 
 test('comparator', (t) => {
-  const c = `${s()}<${s()}1.2.3${s()}`
-  t.equal(new Comparator(c).value, '<1.2.3')
+  const comparator = `${wsLarge}<${wsLarge}1.2.3${wsLarge}`
+  t.equal(new Comparator(comparator).value, '<1.2.3')
   t.end()
 })
