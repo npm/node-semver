f547cf6064f1967892bca3320348e18f89dc6817
Luke Karrys <luke@lukekarrys.com>
2023-07-06 18:10:17 -0700

    fix: correctly parse long build ids as valid
    Fixes #580

--- test/functions/valid.js
+++ test/functions/valid.js
@@ -2,6 +2,7 @@ const t = require('tap')
 const valid = require('../../functions/valid')
 const SemVer = require('../../classes/semver')
 const invalidVersions = require('../fixtures/invalid-versions')
+const { MAX_SAFE_INTEGER } = require('../../internal/constants')
 
 t.test('returns null instead of throwing when presented with garbage', t => {
   t.plan(invalidVersions.length)
@@ -15,5 +16,14 @@ t.test('validate a version into a SemVer object', t => {
   t.equal(valid(s), '4.5.6', 'return the version if a SemVer obj')
   t.equal(valid('4.2.0foo', true), '4.2.0-foo', 'looseness as a boolean')
   t.equal(valid('4.2.0foo', { loose: true }), '4.2.0-foo', 'looseness as an option')
+  t.end()
+})
+
+t.test('long build id', t => {
+  const longBuild = '-928490632884417731e7af463c92b034d6a78268fc993bcb88a57944'
+  const shortVersion = '1.1.1'
+  const longVersion = `${MAX_SAFE_INTEGER}.${MAX_SAFE_INTEGER}.${MAX_SAFE_INTEGER}`
+  t.equal(valid(shortVersion + longBuild), shortVersion + longBuild)
+  t.equal(valid(longVersion + longBuild), longVersion + longBuild)
   t.end()
 })
