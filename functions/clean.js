fb50ab26f964a81525e0c4dd5173ae98107eb34b
lijianan <574980606@qq.com>
2024-09-06 20:05:02 +0800

    Update clean.js

--- functions/clean.js
+++ functions/clean.js
@@ -1,6 +1,6 @@
 const parse = require('./parse')
 const clean = (version, options) => {
-  const s = parse(version.trim().replace(/^[=v]+/, ''), options)
+  const s = parse(version.trim().replace(/^[=v]+/i, ''), options)
   return s ? s.version : null
 }
 module.exports = clean
