0ac2c7129de2d449d55dcdd1becbe658d4148125
Patrick Johnmeyer <pjohnmeyer@gmail.com>
2026-05-02 18:19:22 -0500

    Remove build info for pre* truncation

--- functions/truncate.js
+++ functions/truncate.js
@@ -3,26 +3,35 @@
 const parse = require('./parse')
 const constants = require('../internal/constants')
 
-const _truncate = (version, releaseType) => {
+const _truncate = (version, truncation) => {
+  if (truncation.startsWith('pre')) {
+    return version.version
+  }
+
   version.prerelease = []
 
-  switch (releaseType) {
+  switch (truncation) {
     case 'major':
       version.minor = 0
-    // eslint-disable-next-line no-fallthrough -- this is intentional fallthrough
+      version.patch = 0
+      break
     case 'minor':
       version.patch = 0
+      break
   }
 
   return version.format()
 }
 
-const truncate = (version, releaseType, options) => {
-  if (!constants.RELEASE_TYPES.includes(releaseType)) {
+const truncate = (version, truncation, options) => {
+  if (!constants.RELEASE_TYPES.includes(truncation)) {
     return null
   }
 
   const parsed = parse(version, options)
-  return parsed && _truncate(parsed, releaseType)
+  return parsed && _truncate(parsed, truncation)
 }
 module.exports = truncate
+
+
+// 1. Should truncate _always_ strip off build info?
--- test/functions/truncate.js
+++ test/functions/truncate.js
@@ -4,17 +4,34 @@ const { test } = require('tap')
 const truncate = require('../../functions/truncate')
 const parse = require('../../functions/parse')
 const truncations = require('../fixtures/truncations.js')
+const validVersions = require('../fixtures/valid-versions.js')
 
-test('truncate versions test', (t) => {
-  truncations.forEach(([pre, what, wanted, options, id, base]) => {
-    const found = truncate(pre, what, options, id, base)
-    const cmd = `truncate(${pre}, ${what}, ${options}, ${id}, ${base})`
-    t.equal(found, wanted, `${cmd} === ${wanted}`)
+test('truncate fixture versions test', (t) => {
+  truncations.forEach(([pre, truncation, expected, options, id, base]) => {
+    const actual = truncate(pre, truncation, options, id, base)
+    const cmd = `truncate(${pre}, ${truncation}, ${options}, ${id}, ${base})`
+    t.equal(actual, expected, `${cmd} === ${expected}`)
 
     const parsed = parse(pre, options)
     const semverTruncated = truncate(parsed, what, options)
-    t.equal(semverTruncated, wanted, `${cmd} works on Semver objects`)
+    t.equal(semverTruncated, expected, `${cmd} works on Semver objects`)
   })
 
+  t.end()
+})
+
+test('truncate pre* only removes build info', (t) => {
+  ['prerelease', 'prepatch', 'preminor', 'premajor'].forEach(what => {
+    validVersions.forEach((v) => {
+      const versionToTruncate = v[0]
+      const parsed = parse(versionToTruncate)
+      const expected = parsed.version
+      const actual = truncate(versionToTruncate, what)
+
+      const cmd = `truncate(${versionToTruncate}, ${what})`
+      t.equal(actual, expected, `${cmd} === ${expected}`)
+      t.same(parse(actual).build, [], `${cmd} strips build info `)
+    })
+  })
   t.end()
 })
