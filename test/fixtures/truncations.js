d8bad09d3b30193b074d4e46ead76fdfa3890e49
Patrick Johnmeyer <pjohnmeyer@gmail.com>
2026-05-06 20:24:13 -0500

    Add more prerelease+build test fixtures for truncate

--- test/fixtures/truncations.js
+++ test/fixtures/truncations.js
@@ -11,4 +11,29 @@ module.exports = [
   // invalid inputs
   ['1.2.3', 'fake', null],
   ['fake', 'major', null],
+
+  // additional pre-release, build, and pre+build inputs
+  ['4.5.6-rc2', 'prerelease', '4.5.6-rc2'],
+  ['4.5.6-rc2', 'prepatch', '4.5.6-rc2'],
+  ['4.5.6-rc2', 'preminor', '4.5.6-rc2'],
+  ['4.5.6-rc2', 'premajor', '4.5.6-rc2'],
+  ['4.5.6-rc2', 'patch', '4.5.6'],
+  ['4.5.6-rc2', 'minor', '4.5.0'],
+  ['4.5.6-rc2', 'major', '4.0.0'],
+
+  ['4.5.6+dadb0d', 'prerelease', '4.5.6'],
+  ['4.5.6+dadb0d', 'prepatch', '4.5.6'],
+  ['4.5.6+dadb0d', 'preminor', '4.5.6'],
+  ['4.5.6+dadb0d', 'premajor', '4.5.6'],
+  ['4.5.6+dadb0d', 'patch', '4.5.6'],
+  ['4.5.6+dadb0d', 'minor', '4.5.0'],
+  ['4.5.6+dadb0d', 'major', '4.0.0'],
+
+  ['4.5.6-rc2+dadb0d', 'prerelease', '4.5.6-rc2'],
+  ['4.5.6-rc2+dadb0d', 'prepatch', '4.5.6-rc2'],
+  ['4.5.6-rc2+dadb0d', 'preminor', '4.5.6-rc2'],
+  ['4.5.6-rc2+dadb0d', 'premajor', '4.5.6-rc2'],
+  ['4.5.6-rc2+dadb0d', 'patch', '4.5.6'],
+  ['4.5.6-rc2+dadb0d', 'minor', '4.5.0'],
+  ['4.5.6-rc2+dadb0d', 'major', '4.0.0'],
 ]
